399564052ce5d7dc9e85334174862c421d0b0ef6
Jan Schiefer <janschiefer@users.noreply.github.com>
2022-11-05 01:18:06 +0100

    Use Rust STM32F401 HAL library

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,41 @@
-#![no_std]
+#![deny(unsafe_code)]
+#![allow(clippy::empty_loop)]
 #![no_main]
+#![no_std]
 
-use panic_halt as _;
-use cortex_m::peripheral::syst::SystClkSource;
+// Halt on panic
+use panic_halt as _; // panic handler
+
 use cortex_m_rt::entry;
-use stm32f4::stm32f401 as pac;
+use stm32f4xx_hal as hal;
 
+use crate::hal::{pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
-    // Get access to the core peripherals from the cortex-m crate
-    let cp = cortex_m::Peripherals::take().unwrap();
-    // Get access to the device specific peripherals from the peripheral access crate
-    let dp = pac::Peripherals::take().unwrap();
-    let rcc = dp.RCC;
+    if let (Some(dp), Some(cp)) = (
+        pac::Peripherals::take(),
+        cortex_m::peripheral::Peripherals::take(),
+    ) {
 
-    // Set up the SysTick peripheral.
-    let mut syst = cp.SYST;
-    syst.set_clock_source(SystClkSource::Core);
-    syst.set_reload(2_100_000); //internal in clock ticks
-    syst.enable_counter();
+        let gpioc = dp.GPIOC.split();
+        let mut led = gpioc.pc13.into_push_pull_output();
 
-    // Enable clock on port C
-//  rcc.apb2enr.write(|w| w.iopcen().set_bit());        //stm32f1 
-    rcc.ahb1enr.write(|w| w.gpiocen().set_bit());       //stm32f4
+        // Set up the system clock. We want to run at 84MHz for this one.
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(84.MHz()).freeze();
 
-    let gpioc = dp.GPIOC;
+        // Create a delay abstraction based on SysTick
+        let mut delay = cp.SYST.delay(&clocks);
 
-    // Set pin C13 as general purpose output, with push/pull mode, 2MHz max
-    //gpioc.apb2.write(|w| w.cnf13().bits(0b00)
-    //                      .mode13().bits(0b10));     //stm32f1
-
-    //gpioc.moder.write(|w| w.moder13().bits(0x01));      //General purpose output mode
-    gpioc.moder.write(|w| w.moder13().output());        
-    //gpioc.otyper.write(|w| w.ot13().clear_bit());       //Output push-pull
-    gpioc.otyper.write(|w| w.ot13().push_pull());
-    //gpioc.ospeedr.write(|w| w.ospeedr13().bits(0x00));  //low speed
-    gpioc.ospeedr.write(|w| w.ospeedr13().low_speed());
-    //gpioc.pupdr.write(|w| w.pupdr13().bits(0x00));      //No pull-up, pull-down (not needed, since we use Output: push-pull)    
-    gpioc.pupdr.write(|w| w.pupdr13().floating());        //helper method used instead of .bits() which is marked unsafe
-    
-    
-    // Restart the SysTick counter.
-    syst.clear_current();
-
-    loop {
-        // Toggle the LED every SysTick tick.
-        while !syst.has_wrapped() {};
-        gpioc.odr.write(|w| w.odr13().set_bit());
-        while !syst.has_wrapped() {};
-        gpioc.odr.write(|w| w.odr13().clear_bit());
+        loop {
+            // On for 1s, off for 1s.
+            led.set_high();
+            delay.delay_ms(1000_u32);
+            led.set_low();
+            delay.delay_ms(1000_u32);
+        }
     }
+
+    loop {}
 }
